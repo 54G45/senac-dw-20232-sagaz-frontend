4f85f0e2785c89a19dacf643c34498cfec9cad2b
54G45 <sagas__@outlook.com>
2023-07-27 10:50:31 -0300

    primeiro commit

---
+++ js/exemplos.js
@@ -0,0 +1,8 @@
+
+let prompt = require('prompt-sync')();
+
+let curso = prompt ('Qual o Nome do seu Curso?');
+console.log('Curso: ' + curso);
+
+let fase = Number(prompt('Informe sua Fase: '));
+console.log('Fase: ' + fase);
